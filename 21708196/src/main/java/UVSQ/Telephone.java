0d0edb453e6169358b0170868cca2788fc3c1e8c
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-11 19:52:19 +0100

    Create Telephone.java

---
+++ 21708196/src/main/java/UVSQ/Telephone.java
@@ -0,0 +1,12 @@
+package UVSQ;
+
+public class Telephone {
+	private String nom;
+	private String telephone;
+
+
+public void NumeroTelephone(String nom, String telephone) {
+	this.nom = nom;
+	this.telephone = telephone;
+}
+}
