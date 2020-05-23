4c8206d85b642d52f08c63608d8ec26a6b46ebbc
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 19:54:59 +0200

    Update Person.java
    chkstyle

--- 21708196/src/main/java/UVSQ/Person.java
+++ 21708196/src/main/java/UVSQ/Person.java
@@ -1,102 +1,165 @@
 package UVSQ;
-
-
 import java.time.LocalDate;
 import java.util.ArrayList;
-
 import java.util.List;
-import java.util.Map;
-
-import java.time.format.DateTimeFormatter;
-
-import java.util.stream.Collectors;
-
+/**
+ *class  Person .
+ * @author HAMMOUCHE Koussaila.
+ */
 public final class Person {
-
-	
-	
-	
+/**
+	 *nom: le nom de  Person . 
+	 */
 	private final String nom;
+	/**
+	 *prenom: le prenom de  Person .
+	 */
 	private final String prenom;
+	/**
+	 *fonction: le fonction de  Person .
+	 */
 	private final Fonction fonction;
+	/**
+	 *dateNaissance: le dateNaissance de  Person .
+	 */
 	private final LocalDate dateNaissance;
+	/**
+	 *numerosTelephone: la liste des numerosTelephone de  Person .
+	 */
 	private final List<Telephone> numerosTelephone;
-	
+	/**
+	 *la PersonBuilder  .
+	 */
 	public static class PersonBuilder {
-		
-		
-		private final String nom;
+		/**
+		 *le nom  .
+		 */
+        private final String nom;
+        /**
+		 *le prenom  .
+		 */
 		private final String prenom;
+		/**
+		 *la fonction   .
+		 */
 		private final Fonction fonction;
-		
-		
+		/**
+		 *la dateNaissance  . 
+		 */
 		private LocalDate dateNaissance = null;
+		/**
+		 *numerosTelephone la liste de numeros  .
+		 */
 		private List<Telephone> numerosTelephone = new ArrayList<Telephone>();
-		
+		/**
+		 *la PersonBuilder avec nom prenom et fonction
+		 *@param nom ..
+		 *@param prenom ..
+		 *@param fonction ..
+		 */
 		public PersonBuilder(String nom, String prenom, Fonction fonction) {
+			/**
+			 *le nom  .
+			 */
 			this.nom = nom;
+			/**
+			 *le prenom  .
+			 */
 			this.prenom = prenom;
+			/**
+			 *la fonction   .
+			 */
 			this.fonction = fonction;
 		}
-		
+		/**
+		 *le builder de dateNaissance   .
+		 *@param dateNaissance dateNaissance ..
+		 *@return this dateNaissance
+		 */
 		public PersonBuilder dateNaissance(LocalDate dateNaissance) {
 			this.dateNaissance = dateNaissance;
 			return this;
 		}
-		
-		public PersonBuilder addNumeroTelephone(Telephone numeroTelephone) {
+		/**
+		 *le builder de addNumeroTelephone   .
+		 *@param numeroTelephone le telphone ..
+		 *@return this numerosTelephone
+		 */
+	public PersonBuilder addNumeroTelephone(Telephone numeroTelephone) {
 			this.numerosTelephone.add(numeroTelephone);
 			return this;
 		}
-		
+	/**
+	 *le builder de peson   .
+	 *@return person(i) .
+	 */
 		public Person build() {
 			return new Person(this);
 		}
 	}
-	
+	/**
+	 *le builder de peson   .
+	 *@param builder ..
+	 */
 	private Person(PersonBuilder builder) {
+		/**
+		 *le prenom  .
+		 */
 		nom = builder.nom;
+		/**
+		 *le prenom  .
+		 */
 		prenom = builder.prenom;
+		/**
+		 *la fonctio, .
+		 */
 		fonction = builder.fonction;
+		/**
+		 * la dateNaissance
+		 *  */
 		dateNaissance = builder.dateNaissance;
+		/**
+		 * la numerosTelephone
+		 **/
 		numerosTelephone = builder.numerosTelephone;
 	}
-
-	
-
-	
+	/**
+	 * la methode getNom
+	 * qui retourn le nom
+	 * @return nom  .
+	 **/
 	public String getNom() {
 		return nom;
 	}
-
+	/**
+	 * la methode getPrenom
+	 * qui retourn le prenom
+	 * @return prenom  .
+	 **/
 	public String getPrenom() {
 		return prenom;
 	}
-
-
+	/**
+	 * la methode getFonction
+	 * qui retourn la fonction
+	 * @return prenom  .
+	 **/
 	public Fonction getFonction() {
 		return fonction;
-	}
-
-	
-
-
-	
+		}/**
+	 * la methode getNumerosTelephone
+	 * qui retourn la liste des Numeros d Telephone
+	 * @return prenom  .
+	 **/
 	public List<Telephone> getNumerosTelephone() {
 		return numerosTelephone;
 	}
-
-
-
-
+	/**
+	 * la methode getDateNaissance
+	 * qui retourn la dateNaissance
+	 * @return dateNaissance  .
+	 **/
 	public LocalDate getDateNaissance() {
 		return dateNaissance;
 	}
-	
-
-	
-	
-	
-
-
-}
+	}
