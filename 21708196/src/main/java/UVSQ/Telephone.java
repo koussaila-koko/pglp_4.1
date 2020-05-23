507ba0dd96dbecf09bbed2b60ff4cfe73a53713f
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 19:54:26 +0200

    Update Telephone.java

--- 21708196/src/main/java/UVSQ/Telephone.java
+++ 21708196/src/main/java/UVSQ/Telephone.java
@@ -1,10 +1,22 @@
 package UVSQ;
-
+/**
+ *class  Telephone .  
+ * @author HAMMOUCHE Koussaila.
+ */
 public class Telephone {
+	/**
+	 *le nom de telphone
+	 */
 	private String nom;
+	/**
+	 *le num d Telephon
+	 */
 	private String telephone;
-
-
+     /**
+	 *constructeur de NumeroTelephone.
+	 * @param nom :le nom de telephone
+	 * @param telephone :le num de telepone
+	 */
 public void NumeroTelephone(String nom, String telephone) {
 	this.nom = nom;
 	this.telephone = telephone;
