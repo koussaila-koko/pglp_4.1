0752ea5ae6f793ea9d23eec5ec6ed714d4197370
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 19:55:33 +0200

    Update App.java
    chkstyle

--- 21708196/src/main/java/UVSQ/App.java
+++ 21708196/src/main/java/UVSQ/App.java
@@ -1,13 +1,10 @@
 package UVSQ;
-
 /**
- * Hello world!
- *
+ * la classe app.
+ * @author koussiala HAMMOUCHE .
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App
+ {
+	public static void main(String[] args) {
+        System.out.println("Hello World!"); }
     }
-}
